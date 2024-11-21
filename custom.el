d4a24e1e48d363c4d4d84c07b8ba38bee226b55d
mattiasdrp <mattias@kojin.tech>
2024-11-21 16:15:37 +0100

    elpaca and custom fonts

--- custom.el
+++ custom.el
@@ -22,6 +22,25 @@
  '(package-selected-packages nil)
  '(pokemacs-theme 'doom-solarized-dark)
  '(show-paren-style 'expression)
+ '(tuple-mono-font
+   (if (window-system)
+       (cond
+        ((x-list-fonts "Fira Code") '(:font "Fira Code"))
+        ((x-list-fonts "Inconsolata") '(:font "Inconsolata"))
+        ((x-family-fonts "DejaVu") '(:family "DejaVu"))
+        (nil (warn "Cannot find a monospaced font.")))
+     '(:family "Monospace")) t)
+ '(tuple-variable-font
+   (if (window-system)
+       (cond
+        ((x-list-fonts "Iosevka Aile") '(:font "Iosevka Aile"))
+        ((x-list-fonts "ETBembo") '(:font "ETBembo"))
+        ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+        ((x-list-fonts "Lucida Grande") '(:font "Lucida Grande"))
+        ((x-list-fonts "Verdana") '(:font "Verdana"))
+        ((x-family-fonts "Sans Serif") '(:family "Sans Serif"))
+        (nil (warn "Cannot find a Sans Serif Font.")))
+     '(:family "Monospace")) t)
  '(vertico-multiform-categories
    '((imenu buffer) (file (vertico-sort-function . sort-directories-first))
      (corfu (vertico-sort-function . vertico-sort-alpha))
@@ -44,10 +63,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fira Code" :slant normal :weight normal :height 118 :width normal :foundry "CTDB"))))
- '(fixed-pitch ((t (:family "Fira Code" :slant normal :weight normal :height 118 :width normal :foundry "CTDB"))))
+ `(default ((t (,@tuple-mono-font :slant normal :weight normal :height 118 :width normal :foundry "CTDB"))))
+ `(fixed-pitch ((t (,@tuple-mono-font :slant normal :weight normal :height 118 :width normal :foundry "CTDB"))))
  '(tuareg-font-lock-constructor-face ((t (:inherit font-lock-type-face))))
- '(variable-pitch ((t (:font "Iosevka Aile" :weight light :height 1.3)))))
+ `(variable-pitch ((t (,@tuple-variable-font :weight light :height 1.3)))))
 
 (provide 'custom)
 ;;; custom.el ends here
--- init.el
+++ init.el
@@ -1,14 +1,13 @@
 ;;; Configure Elpaca -----------------------------
 
-(setq elpaca-core-date '(20240411))
-(defvar elpaca-installer-version 0.7)
+(defvar elpaca-installer-version 0.8)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-			                        :ref nil :depth 1
-			                        :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-			                        :build (:not elpaca--activate-package)))
+                              :ref nil :depth 1
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))
@@ -18,20 +17,20 @@
     (make-directory repo t)
     (when (< emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
-	      (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-		             ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
-						                                     ,@(when-let ((depth (plist-get order :depth)))
-						                                         (list (format "--depth=%d" depth) "--no-single-branch"))
-						                                     ,(plist-get order :repo) ,repo))))
-		             ((zerop (call-process "git" nil buffer t "checkout"
-				                               (or (plist-get order :ref) "--"))))
-		             (emacs (concat invocation-directory invocation-name))
-		             ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-				                               "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-		             ((require 'elpaca))
-		             ((elpaca-generate-autoloads "elpaca" repo)))
-	          (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-	        (error "%s" (with-current-buffer buffer (buffer-string))))
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
       ((error) (warn "%s" err) (delete-directory repo 'recursive))))
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
@@ -83,18 +82,6 @@
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
-
-
-;;; Org Mode Appearance ------------------------------------
-
-;; Load org-faces to make sure we can set appropriate faces
-(use-package org
-  :disabled
-  :ensure t
-  :mode ("\\.org\\'" . org-mode)
-  :config
-  ;; Hide emphasis markers on formatted text
-  (setq org-hide-emphasis-markers t))
 
 ;; Appearance etc
 
